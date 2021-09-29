dc4d55de50eefe5c8714b7da7d737bde49a1812f
ramalakshmi2020 <ramalakshmi.n@gmail.com>
2021-09-29 12:14:27 -0700

    commit

---
+++ SimpleProblems/DigitSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DigitSum
+{
+    class Program
+    {
+        static int DigitSum(int number)
+        {
+            if (number / 10 == 0)
+            {
+                return number;
+            }
+            return number % 10 + DigitSum(number / 10);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a valid integer whose sum of digits to be computed :");
+            int number;
+            bool valid = int.TryParse(Console.ReadLine(), out number);
+
+            while (!valid)
+            {
+                Console.WriteLine("Enter a valid integer ");
+                valid = int.TryParse(Console.ReadLine(), out number);
+            }
+            
+            Console.WriteLine("The sum of the digits of number {0} is {1}", number, DigitSum(Math.Abs(number)));
+
+        }
+    }
+}
