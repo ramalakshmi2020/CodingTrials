9bd0b704b1ca0240006e0d63d4e03353daffbd09
ramalakshmi2020 <ramalakshmi.n@gmail.com>
2021-10-07 10:47:57 -0700

    Update Program.cs

--- DataStructures/LRUCache/LRUCache/Program.cs
+++ DataStructures/LRUCache/LRUCache/Program.cs
@@ -5,7 +5,7 @@ namespace LRUCache
 {
     class Program
     {
-        public class LRUCache1
+        public class LRUCache
         {
             private int size;
             private Dictionary<int, Node> map;
@@ -19,7 +19,7 @@ namespace LRUCache
                 public Node prev;
                 public Node next;
             }
-            public LRUCache1(int Size)
+            public LRUCache(int Size)
             {
 
                 size = Size;
@@ -92,7 +92,7 @@ namespace LRUCache
 
         }
 
-        public class LRUCache
+        public class LRUCachewithoutDummyHeadandTail
         {
             private int size;
             private Dictionary<int, Node> map;
@@ -106,7 +106,7 @@ namespace LRUCache
                 public Node prev;
                 public Node next;
             }
-            public LRUCache(int Size)
+            public LRUCachewithoutDummyHeadandTail(int Size)
             {
 
                 size = Size;
@@ -223,7 +223,7 @@ namespace LRUCache
             Console.WriteLine("Hello World!");
             // ["LRUCache","put","put","get","put","get","put","get","get","get"]
             //[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
-            LRUCache1 cache = new LRUCache1(3);
+            LRUCache cache = new LRUCache(3);
             cache.Set(1,1);
             cache.Set(2, 2);
             Console.WriteLine("Result of Get(1) after setting [1,1],[2,2] is {0} ", cache.Get(1));
@@ -238,12 +238,12 @@ namespace LRUCache
             
             Console.WriteLine("Result of Get(1)  {0} ", cache1.Get(2));
             cache1.Set(2, 6);
-            Console.WriteLine("Result of Get(2) after setting [1,1],[2,2][3,3] is {0} ", cache1.Get(2));
+            Console.WriteLine("Result of Get(2) after setting [2, 6] is {0} ", cache1.Get(2));
             cache1.Set(1, 5);
             cache1.Set(1, 2);
-            Console.WriteLine("Result of Get(1) (it got evicted  already) is {0} ", cache1.Get(1));
-            Console.WriteLine("Result of Get(3) is {0} ", cache1.Get(2));
-            //Console.WriteLine("Result of Get(3) is {0} ", cache.Get(4));
+            Console.WriteLine("Result of Get(1) after  [1, 5][1,2] is {0} ", cache1.Get(1));
+            Console.WriteLine("Result of Get(2) is {0} ", cache1.Get(2));
+          
 
 
         }
